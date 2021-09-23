1af17110ea9a881bfcc5e89e839b8d142718e939
Dimo Dimchev <ddimchev7@gmail.com>
2021-09-23 19:19:01 +0300

    Added simple home page to test login functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import LogInButton from "./components/LogInButton";
+import Profile from "./components/Profile";
 
+// Simple Home page to display the log in button and user info if the user is logged in
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <LogInButton />
+      <Profile />
+    </>
+  );
 }
 
 export default App;
