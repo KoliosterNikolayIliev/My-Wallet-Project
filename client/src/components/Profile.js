9ed1bb9d367b7b4ed8d1a5db9c9b239a9e90bd20
Dimo Dimchev <ddimchev7@gmail.com>
2021-09-24 21:12:40 +0300

    Added Profile page. Functionality to be added later

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,19 +1,27 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import LogOutButton from "./LogOutButton";
+import { Redirect } from "react-router";
 
-// Profile component to display user information. Purely for demonstration purposes. Not intended for use in the app.
+// Profile component to display user information.
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
+  //   Return this if Auth0 is still loading. Can be replaced with an animation in the future
   if (isLoading) {
     return <div>Loading ...</div>;
+  }
+
+  // Redirect the user to the landing page if the user is not logged in
+  if (!isAuthenticated) {
+    return <Redirect to={"/"} />;
   }
 
   return (
     isAuthenticated && (
       <div>
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
+        <h2>Hi, {user.name}, this is the profile page</h2>
+        <LogOutButton />
       </div>
     )
   );
