fad62aa543a522252d00c2a3c2fccd5e21b46c14
Dimo Dimchev <ddimchev7@gmail.com>
2021-09-24 21:11:46 +0300

    Set up router using React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,32 @@
 import "./App.css";
-import LogInButton from "./components/LogInButton";
-import LogOutButton from "./components/LogOutButton";
+import Dashboard from "./components/Dashboard";
 import Profile from "./components/Profile";
+import Landing from "./components/Landing";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 // Simple Home page to display the log in button and user info if the user is logged in
 function App() {
+  const { isAuthenticated } = useAuth0();
+
   return (
-    <>
-      <LogInButton />
-      <LogOutButton />
-      <Profile />
-    </>
+    // Wrap app in a router to handle routing
+    <Router>
+      {/* Add switch for react router */}
+      <Switch>
+        {/* If the user is logged in directly redirect them to the dashboard, otherwise show the landing page */}
+        <Route path="/" exact>
+          {isAuthenticated ? <Redirect to="/dashboard" /> : <Landing />}
+        </Route>
+        <Route path="/profile" exact component={Profile} />
+        <Route path="/dashboard" exact component={Dashboard} />
+      </Switch>
+    </Router>
   );
 }
 
