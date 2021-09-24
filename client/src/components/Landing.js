d274efde9509be5eaa2db99f9f40c80c54479e38
Dimo Dimchev <ddimchev7@gmail.com>
2021-09-24 21:12:20 +0300

    Added Landing page. Design to be added later

---
+++ client/src/components/Landing.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import LogInButton from "./LogInButton";
+
+// Landing page to explain our product. Includes a button to log in.
+const Landing = () => {
+  const { isLoading } = useAuth0();
+
+  //   Return this if Auth0 is still loading. Can be replaced with an animation in the future
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+    <>
+      <LogInButton />
+      <h2>This is the landing page</h2>
+    </>
+  );
+};
+
+export default Landing;
