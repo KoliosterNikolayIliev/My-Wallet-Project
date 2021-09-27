3144972f6f3749196aeca7181fdf6ee4080dd673
Dimo Dimchev <ddimchev7@gmail.com>
2021-09-27 17:29:56 +0300

    Created blueprint for early access form component

---
+++ campaign/src/components/EarlyAccessComponent.js
@@ -0,0 +1,10 @@
+const EarlyAccessComponent = () => {
+  return (
+    <form>
+      <input type="email" placeholder="Your email..." />
+      <input type="submit" value="Get early access" />
+    </form>
+  );
+};
+
+export default EarlyAccessComponent;
