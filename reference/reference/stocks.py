7b8cfba68727709de4442e6b6140ef63a37d05a9
Dimo Dimchev <ddimchev7@gmail.com>
2021-10-02 15:01:22 +0300

    Rename variables
    Renamed most variables in stocks.py to meet the standard variable naming rules I selected for this microservice

--- reference/reference/stocks.py
+++ reference/reference/stocks.py
@@ -6,36 +6,36 @@ from .extensions import scheduler
 STOCKS_LIST = ["AMZN", "MSFT", "GOOGL", "AMD", "MRNA", "TSLA", "PLTR", "AAPL"]
 
 # store latest prices for shortlisted stocks
-prices_store = {}
+stocks_prices_store = {}
 
 # create blueprint
 bp = Blueprint('stocks', __name__, url_prefix='/stocks')
 
 # fetch real-time price data for shortlisted stocks
-def fetch_price_data(symbols: list):
-    global prices_store
+def fetch_stocks_price_data(symbols: list):
+    global stocks_prices_store
     symbols = (',').join(symbols)
     req = requests.get('https://api.twelvedata.com/price', params={'symbol': symbols, 'apikey': os.environ['TD_API_KEY'], 'interval': '1min'})
-    req = req.json()
+    res = req.json()
 
-    # if there is an error, print the error message, else update the prices_store
+    # if there is an error, print the error message, else update the stocks_prices_store
     try:
-        for res_key, res_value in req.items():
-            prices_store[res_key] = res_value['price']
+        for res_key, res_value in res.items():
+            stocks_prices_store[res_key] = res_value['price']
     except:
-        print(f"Error: {req['message']}")
+        print(f"Error: {res['message']}")
         
 
 # fetch price data for shortlisted stocks the first time the server goes live
-scheduler.add_job(func=fetch_price_data, args=[STOCKS_LIST], id='price_update', max_instances=1)
+scheduler.add_job(func=fetch_stocks_price_data, args=[STOCKS_LIST], id='stocks_price_update', max_instances=1)
 
 # fetch price data for shortlisted stocks every 24 hours
-scheduler.add_job(func=fetch_price_data, args=[STOCKS_LIST], trigger='interval', seconds=86400, id='periodic_price_update')
+scheduler.add_job(func=fetch_stocks_price_data, args=[STOCKS_LIST], trigger='interval', seconds=86400, id='periodic_stocks_price_update')
 
 # microservice endpoint to return the latest prices for shortlisted stocks
 @bp.route('/prices', methods=(['GET']))
 def prices():
-    if prices_store:
-        return jsonify(prices_store)
-    fetch_price_data(STOCKS_LIST)
-    return jsonify(prices_store)
+    if stocks_prices_store:
+        return jsonify(stocks_prices_store)
+    fetch_stocks_price_data(STOCKS_LIST)
+    return jsonify(stocks_prices_store)
