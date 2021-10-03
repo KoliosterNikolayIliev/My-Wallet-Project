aec00b0cf90624b8b9ce0441e03de7e780ecceee
Sheko1 <martinkypar@gmail.com>
2021-10-04 01:22:13 +0300

    added balances endpoint for nordigen

--- assets/src/app.py
+++ assets/src/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+
+from src.blueprints.balances import balances
 from src.blueprints.link_accounts import link_accounts
 
 app = Flask(__name__)
 
 app.register_blueprint(link_accounts, url_prefix='/link-accounts')
+app.register_blueprint(balances, url_prefix='/balances')
---
+++ assets/src/blueprints/balances.py
@@ -0,0 +1,29 @@
+from flask import request, Blueprint
+from flask_restful import Resource, Api
+from utils import nordigen
+
+
+balances = Blueprint('balances', __name__)
+api = Api(balances)
+
+
+class GetBalances(Resource):
+    def get(self):
+        data = {}
+        nordigen_key = request.form.get('nordigen')
+
+        if nordigen_key:
+            accounts = nordigen.list_accounts(nordigen_key).get('accounts')
+            if not accounts:
+                data['nordigen'] = 'No bank accounts'
+
+            else:
+                data['nordigen'] = {}
+
+                for account in accounts:
+                    data['nordigen'][account] = nordigen.get_account_balances(account)
+
+        return data
+
+
+api.add_resource(GetBalances, '/')
