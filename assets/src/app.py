259471331001cd94510d0cba05624956d261f185
Sheko1 <martinkypar@gmail.com>
2021-10-04 00:46:27 +0300

    removed GetUserAssets

--- assets/src/app.py
+++ assets/src/app.py
@@ -1,38 +1,6 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
-from utils import nordigen
+from flask import Flask
 from src.blueprints.link_accounts import link_accounts_api
 
 app = Flask(__name__)
-api = Api(app)
 
-
-class GetUserAssets(Resource):
-    def get(self):
-        data = {}
-
-        if request.form.get('nordigen'):
-            accounts = nordigen.list_accounts(request.form['nordigen']).get('accounts')
-            print(nordigen.list_accounts(request.form['nordigen']))
-
-            if not accounts:
-                return {'detail': 'Wrong requisition id', 'status_code': 400}, 400
-
-            data['nordigen'] = []
-
-            if accounts:
-                for account in accounts:
-                    account_data = {}
-                    bank_metadata = nordigen.get_bank_by_id(nordigen.get_account_metadata(account)['aspsp_identifier'])
-                    account_data['bank_name'] = bank_metadata['name']
-                    account_data['bank_logo'] = bank_metadata['logo']
-                    data['nordigen'].append(account_data)
-
-            else:
-                data['nordigen'] = 'No linked bank accounts'
-
-        return data
-
-
-api.add_resource(GetUserAssets, '/user-assets')
 app.register_blueprint(link_accounts_api, url_prefix='/link-accounts')
