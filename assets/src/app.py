5f8b1c6c88286275de6dc0afe17f2ecf0bdf5113
Sheko1 <martinkypar@gmail.com>
2021-10-04 01:04:35 +0300

    fixed imports in app.py

--- assets/src/app.py
+++ assets/src/app.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from src.blueprints.link_accounts import link_accounts_api
+from src.blueprints.link_accounts import link_accounts
 
 app = Flask(__name__)
 
-app.register_blueprint(link_accounts_api, url_prefix='/link-accounts')
+app.register_blueprint(link_accounts, url_prefix='/link-accounts')
