5f076eda06a36b835db73eae905f66678f0c373e
Sheko1 <martinkypar@gmail.com>
2021-10-04 17:17:36 +0300

    removed link-accounts endpoint

--- assets/src/app.py
+++ assets/src/app.py
@@ -1,11 +1,9 @@
 from flask import Flask
 
 from src.blueprints.balances import balances
-from src.blueprints.link_accounts import link_accounts
 from src.blueprints.transactions import transactions
 
 app = Flask(__name__)
 
-app.register_blueprint(link_accounts, url_prefix='/link-accounts')
 app.register_blueprint(balances, url_prefix='/balances')
 app.register_blueprint(transactions, url_prefix='/transactions')
