ae79e3f46a7cd84f7a90bd2a64b047bad026aa21
Dimo Dimchev <ddimchev7@gmail.com>
2021-10-04 21:02:30 +0300

    Made changes to stocks/prices endpoint

--- mock-apis/reference/reference/blueprints/stocks.py
+++ mock-apis/reference/reference/blueprints/stocks.py
@@ -1,4 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 # create blueprint
-bp = Blueprint('crypto', __name__, url_prefix='/crypto')
+bp = Blueprint('stocks', __name__, url_prefix='/stocks')
+
+@bp.route('/prices', methods=['GET'])
+def prices():
+    test_data = {
+        'AAPL': 138.13,
+        'MSFT': 281.24,
+        'PLTR': 23.20,
+        'GOOGL': 2654.24,
+        'AMZN': 3185.66,
+        'MRNA': 325.30,
+        'TSLA': 782.06,
+        'AMD': 100.00,
+    }
+    return jsonify(test_data)
