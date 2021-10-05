cdb0e4e127bd064762487f54fa0f45229f5dfba2
Dimo Dimchev <ddimchev7@gmail.com>
2021-10-05 21:00:04 +0300

    Added logic for getting account balances

--- assets/utils/yodlee.py
+++ assets/utils/yodlee.py
@@ -13,5 +13,19 @@ URL = "https://sandbox.api.yodlee.uk/ysl/"
 def get_access_token(loginName):
     data = {'clientId': CLIENT_ID, 'secret': SECRET}
     headers = {'Api-Version': '1.1', 'loginName': loginName}
-    response = requests.post(URL + 'auth/token', data=data, headers=headers)
-    return response.json()['token']['accessToken']
+    try:
+        response = requests.post(URL + 'auth/token', data=data, headers=headers)
+        return response.json()['token']['accessToken']
+    except:
+        return response.json()['errorMessage']
+
+def get_balances(loginName):
+    data = {}
+    try:
+        headers = {'Api-Version': '1.1', 'Authorization': 'Bearer ' + get_access_token(loginName)}
+        response = requests.get(URL + 'accounts', headers=headers)
+        for account in response.json()['account']:
+            data[account['accountName']] = account['balance']
+        return data
+    except:
+        return 'An error occured'
