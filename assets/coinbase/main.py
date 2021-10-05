7c7221a597d757c0f804797a31ca7de0a6fd53c2
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-10-05 10:50:16 +0300

    Update main.py

--- assets/coinbase/main.py
+++ assets/coinbase/main.py
@@ -1,19 +1,23 @@
 import cbpro
 from flask import Flask, jsonify
 
-# Second API credentials for the accounts:
+app = Flask(__name__)
+
+""" Hardcoded 4 variables that should come from the Admin module """
+account_id = 'b0f26bb3-17ed-4281-8cad-9068ea09a96c'
 api_secret_acc = 'F2xOifdoBXgXu+1CWmlBXO4Y0VM7XuWIiJMPcA55dxY0ddMcBOywpDmbx7KlAJ/kgXkXKRGvvM9AyZsgEuCvHQ=='
 api_key_acc = '96c453261680c5edff567425d54c5014'
 api_pass_acc = 'vl8hxvmfhz'
 
+""" The URL path is currently leading to the test enviroement.
+Once the app is ready, it needs to be changed to:
+'https://api-public.pro.coinbase.com' """
 URL = 'https://api-public.sandbox.pro.coinbase.com'
 API_KEY = api_key_acc
 API_SECRET = api_secret_acc
 API_PASS = api_pass_acc
 
-app = Flask(__name__)
-
-""" Authenticate the client """
+""" Authenticate the client via the cbpro library"""
 client = cbpro.AuthenticatedClient(
     key=API_KEY,
     b64secret=API_SECRET,
@@ -21,45 +25,27 @@ client = cbpro.AuthenticatedClient(
     api_url=URL
 )
 
-
-all_clients = client.get_accounts()  # method to check all existing clients
-
-"""
-Our test client is with ID "b0f26bb3-17ed-4281-8cad-9068ea09a96c"
-"""
-account_id = 'b0f26bb3-17ed-4281-8cad-9068ea09a96c'
-
-""" Use this endpoint when you know the account_id. """
 client_data = client.get_account(account_id)
 
-# @app.route('/account', methods=['GET'])
-# def get():
-#     return jsonify({"Client data": client_data})
+""" Get client's current balance """
 
 
+@app.route('/balances', methods=['GET'])
+def get_account():
+    return jsonify({"Client data": client_data})
+
+
+""" Get client's all transactions """
 client_history = client.get_account_history(account_id)
-current_holdings = client.get_account_holds(account_id)
 
 client_history_list = []
-
 for each in client_history:
     client_history_list.append(each)
 
 
-@app.route('/')
-def index():
-    return "Welcome"
-
-""" Get all orders from particular clieent """
-@app.route('/orders', methods=['GET'])
-def get():
+@app.route('/transactions', methods=['GET'])
+def get_orders():
     return jsonify({"Client orders": client_history_list})
-
-
-""" Get all orders by id """
-@app.route('/orders/<int:order_id>', methods=['GET'])
-def get_order(order_id):
-    return jsonify({"Client order": client_history_list[order_id - 1]})
 
 
 if __name__ == '__main__':
