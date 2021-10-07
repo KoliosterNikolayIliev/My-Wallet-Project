78d96dfeb882632e3de9e0f9050a718d415bedff
Dimo Dimchev <ddimchev7@gmail.com>
2021-10-07 21:07:13 +0300

    Set up connection to MongoDB

--- campaign/server/server.js
+++ campaign/server/server.js
@@ -1,11 +1,10 @@
 const express = require("express");
+require("./config/dbConfig");
 
 const app = express();
 const server = require("http").createServer(app);
 
-app.get("/save-form-data", (req, res) => {
-  res.send("Hello World!");
-});
+app.get("/save-form-data", (req, res) => {});
 
 server.listen(5000, () => {
   console.log("Initialized");
