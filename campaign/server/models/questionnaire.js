2e5fdb29ee04e97bf43a74924d2fe7d4648f740f
Dimo Dimchev <ddimchev7@gmail.com>
2021-10-07 21:38:44 +0300

    Added author field to schema

--- campaign/server/models/questionnaire.js
+++ campaign/server/models/questionnaire.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 // create questionaire schema
 const questionnaireSchema = new mongoose.Schema({
+  author: String,
   response1: String,
   response2: String,
   response3: String,
