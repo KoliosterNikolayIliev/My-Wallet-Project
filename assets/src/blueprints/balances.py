372f39a73a5d65eb46b97bf4fbe38bd4ce417ea9
Sheko1 <martinkypar@gmail.com>
2021-10-07 22:56:55 +0300

    added binance functionality for balances endpoint

--- assets/src/blueprints/balances.py
+++ assets/src/blueprints/balances.py
@@ -1,6 +1,6 @@
 from flask import request, Blueprint
 from flask_restful import Resource, Api
-from utils import nordigen, yodlee
+from utils import nordigen, yodlee, binance_api
 
 
 balances = Blueprint('balances', __name__)
@@ -11,7 +11,8 @@ class GetBalances(Resource):
     def get(self):
         data = {
             'nordigen': nordigen.get_account_balances(request.headers.get('nordigen')),
-            'yodlee': yodlee.get_balances(request.headers.get('yodlee_loginName'))
+            'yodlee': yodlee.get_balances(request.headers.get('yodlee_loginName')),
+            'binance': binance_api.get_balances(request.headers.get('binance_key'), request.headers.get('binance_secret')),
         }
 
         return data
