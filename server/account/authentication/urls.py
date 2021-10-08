fe3590b30a8b7cfadb434c3fff229f08bbc372ba
Nikolay Iliev <65191727+KoliosterNikolayIliev@users.noreply.github.com>
2021-10-08 11:24:43 +0300

    #73 reformat endpoints

--- server/account/authentication/urls.py
+++ server/account/authentication/urls.py
@@ -6,6 +6,6 @@ from authentication.views.get_and_create_view import get_and_create_user_profile
 
 urlpatterns = [
     path('user/', get_and_create_user_profile, name="user_details"),
-    path('user/edit', edit_user_profile, name="delete_user"),
-    path('user/delete', delete_user_account, name="delete_user"),
+    path('user/edit/', edit_user_profile, name="delete_user"),
+    path('user/delete/', delete_user_account, name="delete_user"),
 ]
