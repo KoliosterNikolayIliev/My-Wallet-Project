d455947fa0d196715b1806a48a0e99e9e5584137
Nikolay Iliev <65191727+KoliosterNikolayIliev@users.noreply.github.com>
2021-10-08 13:06:31 +0300

    update requirements.txt

--- server/account/account/settings.py
+++ server/account/account/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_jwt',
     'corsheaders',
     'authentication',
 ]
