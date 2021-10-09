6be4c03823f6a27fa60eeced29779e2cc8450e98
Dimo Dimchev <ddimchev7@gmail.com>
2021-10-09 13:16:01 +0300

    Added functionality for internal calls to Reference

--- portfolio/portfolio/utils/reference.py
+++ portfolio/portfolio/utils/reference.py
@@ -1,5 +1,11 @@
-from flask import jsonify
 import requests
 
 
-# reference functionality here
+def get_crypto_prices():
+    res = requests.get("http://127.0.0.1:5002/crypto/prices")
+    return res.json()
+
+
+def get_stocks_prices():
+    res = requests.get("http://127.0.0.1:5002/stocks/prices")
+    return res.json()
