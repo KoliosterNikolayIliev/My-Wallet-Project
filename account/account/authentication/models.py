529c7db7b8e9c39ae3842453481a77d6e3f52b86
Nikolay Iliev <65191727+KoliosterNikolayIliev@users.noreply.github.com>
2021-10-10 15:32:23 +0300

    #81 Modifying model fields

--- account/account/authentication/models.py
+++ account/account/authentication/models.py
@@ -5,11 +5,13 @@ from encrypted_model_fields.fields import EncryptedCharField
 
 
 class UserProfile(models.Model):
+
     user_identifier = models.CharField(max_length=40, blank=False)
     base_currency = models.CharField(max_length=20, default='GBP')
     source_label = models.CharField(max_length=20, blank=True)
     # All of these will be encrypted fields (https://pypi.org/project/django-encrypted-model-fields/)
     # The rest will be added in future
-    binance_key = models.CharField(max_length=100, blank=True, )
-    binance_secret = models.CharField(max_length=100, blank=True)
-    yodlee_login_name = EncryptedCharField()
+    binance_key = EncryptedCharField(max_length=500, blank=True)
+    binance_secret = EncryptedCharField(max_length=500, blank=True)
+    yodlee_login_name = EncryptedCharField(max_length=500, blank=True)
+    nordigen_requisition = EncryptedCharField(max_length=500, blank=True)
