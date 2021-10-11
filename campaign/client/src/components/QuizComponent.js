299cb5b52b811f51beae33d331ef81dc040b388b
Dimo Dimchev <ddimchev7@gmail.com>
2021-10-11 18:34:30 +0300

    Created QuizComponent to handle logic for each question

---
+++ campaign/client/src/components/QuizComponent.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { useState } from "react";
+
+const QuizComponent = ({
+  quizState,
+  setQuizState,
+  answerSheet,
+  setAnswerSheet,
+  submitFunction,
+  question,
+  answer1,
+  answer2,
+  answer3,
+}) => {
+  const [answerSelected, setAnswerSelected] = useState(null);
+
+  const selectAnswer = (e) => {
+    // remove the selected class from all sibling elements
+    e.target.parentElement.childNodes.forEach((node) => {
+      node.classList.remove("selected");
+    });
+
+    // add the selected class to the selected answer
+    e.target.classList.add("selected");
+
+    // add the selected answer to the answer sheet
+    setAnswerSheet({
+      ...answerSheet,
+      [question]: e.target.innerText,
+    });
+    setAnswerSelected(true);
+  };
+
+  // Increment the state of the quiz
+  const nextQuizState = () => {
+    if (quizState < 3) {
+      setQuizState(quizState + 1);
+    }
+  };
+
+  return (
+    <div>
+      <h4>{question}</h4>
+      <p onClick={selectAnswer}>{answer1}</p>
+      <p onClick={selectAnswer}>{answer2}</p>
+      <p onClick={selectAnswer}>{answer3}</p>
+
+      {/* Show the submit button if this is the last question in the quiz */}
+      {answerSelected && quizState === 3 && (
+        <div>
+          <button onClick={submitFunction}>Submit</button>
+        </div>
+      )}
+
+      {answerSelected && quizState !== 3 && (
+        <div>
+          <button onClick={nextQuizState}>Next</button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default QuizComponent;
