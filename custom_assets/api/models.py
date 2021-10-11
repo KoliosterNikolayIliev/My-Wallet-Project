6388096a20a77852d72e490f2d8047d399c0ee00
Sheko1 <martinkypar@gmail.com>
2021-10-11 20:05:16 +0300

    added crypto asset model

--- custom_assets/api/models.py
+++ custom_assets/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+class CryptoAsset(models.Model):
+    custom_assets_key = models.CharField(max_length=50)
+    crypto = models.CharField(max_length=5)
+    amount = models.FloatField()
