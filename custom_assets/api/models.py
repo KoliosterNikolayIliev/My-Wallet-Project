b3bf9c0a489630fd7079f8f956e27d95657ea175
Sheko1 <martinkypar@gmail.com>
2021-10-13 16:33:08 +0300

    added user-assets model with crypto-assets array field and changed django version

--- custom_assets/api/models.py
+++ custom_assets/api/models.py
@@ -4,6 +4,18 @@ from djongo import models
 # Create your models here.
 
 class CryptoAsset(models.Model):
-    custom_assets_key = models.CharField(max_length=50)
-    crypto = models.CharField(max_length=5)
+    type = models.CharField(max_length=5)
     amount = models.FloatField()
+
+    class Meta:
+        abstract = True
+
+
+class UserAssets(models.Model):
+    user_key = models.CharField(
+        max_length=50,
+    )
+
+    crypto_assets = models.ArrayField(
+        model_container=CryptoAsset,
+    )
