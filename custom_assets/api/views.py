804c753fa6bdf0ac33023296d57d9dc84d89cbbf
Sheko1 <martinkypar@gmail.com>
2021-10-13 17:01:17 +0300

    added logic for GetAssets

--- custom_assets/api/views.py
+++ custom_assets/api/views.py
@@ -2,7 +2,8 @@ from rest_framework.generics import CreateAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api.serializers import CryptoAssetSerializer
+from api.models import UserAssets
+from api.serializers import CryptoAssetSerializer, UserAssetsSerializer
 
 
 class CreateCryptoAsset(CreateAPIView):
@@ -11,4 +12,12 @@ class CreateCryptoAsset(CreateAPIView):
 
 class GetAssets(APIView):
     def get(self, request):
-        pass
+        user_key = request.headers.get('user-key')
+
+        if not user_key:
+            return Response('User key was not provided', status=400)
+
+        user_assets = UserAssets.objects.get(user_key=user_key)
+        serializer = UserAssetsSerializer(user_assets)
+
+        return Response(serializer.data)
