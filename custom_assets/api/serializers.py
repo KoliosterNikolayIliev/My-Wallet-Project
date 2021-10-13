7a1649f41e783b3dd726c9ec1ac83e47b6490c31
Sheko1 <martinkypar@gmail.com>
2021-10-13 18:03:36 +0300

    added StockAssetSerializer

--- custom_assets/api/serializers.py
+++ custom_assets/api/serializers.py
@@ -28,6 +28,31 @@ class CryptoAssetSerializer(serializers.Serializer):
         return validated_data
 
 
+class StockAssetSerializer(serializers.Serializer):
+    type = serializers.CharField(max_length=5)
+    amount = serializers.IntegerField()
+
+    def validate(self, attrs):
+        # get user_key from request data, in attrs is only data about serializer fields
+        user_key = self.context['request'].data.get('user-key')
+        validate_user_key(user_key)
+
+        return super().validate(attrs)
+
+    def create(self, validated_data):
+        user_key = self.context['request'].data['user-key']
+        user_assets = UserAssets.objects.get(user_key=user_key)
+
+        # when we create new user asset we pass only user_key so any other field is null
+        if not user_assets.stock_assets:
+            user_assets.stock_assets = []
+
+        user_assets.stock_assets.append(validated_data)
+        user_assets.save()
+
+        return validated_data
+
+
 class UserAssetsSerializer(serializers.ModelSerializer):
     crypto_assets = CryptoAssetSerializer(many=True)
 
