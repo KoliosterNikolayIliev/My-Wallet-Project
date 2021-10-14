e359de36cf8ca9adf99bca25d14fc0fd00db92c1
Dimo Dimchev <ddimchev7@gmail.com>
2021-10-14 09:08:25 +0300

    Updated error messages for Coinbase API

--- assets/utils/coinbase_api.py
+++ assets/utils/coinbase_api.py
@@ -19,8 +19,15 @@ def get_account_balances(api_key, api_secret):
 
 
 def get_transactions(api_key, api_secret):
-    client = Client(api_key, api_secret)
-    client_accounts = client.get_accounts()
+    try:
+        client = Client(api_key, api_secret)
+    except Exception as e:
+        return f"Error: {e}"
+    
+    try:
+        client_accounts = client.get_accounts()
+    except Exception as e:
+        return f"Error: {e.message}"
 
     data = []
     for each_wallet in client_accounts["data"]:
