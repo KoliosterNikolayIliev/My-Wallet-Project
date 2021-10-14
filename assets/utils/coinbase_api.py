fc1532b32192a6f33a17bc3c6f95b4ad6855bc5b
Dimo Dimchev <ddimchev7@gmail.com>
2021-10-14 20:47:03 +0300

    Changed responses for Coinbase

--- assets/utils/coinbase_api.py
+++ assets/utils/coinbase_api.py
@@ -5,34 +5,34 @@ def get_account_balances(api_key, api_secret):
     try:
         client = Client(api_key, api_secret)
     except Exception as e:
-        return f"Error: {e}"
+        return {'status': 'failed', 'content': f"Error: {e}"}
     
     try:
         client_accounts = client.get_accounts()
     except Exception as e:
-        return f"Error: {e.message}"
+        return {'status': 'failed', 'content': f"Error: {e.message}"}
     
     data = []
     for account in client_accounts["data"]:
         data.append({"balance": account["balance"].amount, "currency": account["balance"].currency})
-    return data
+    return {'status': 'success', 'content': data}
 
 
 def get_transactions(api_key, api_secret):
     try:
         client = Client(api_key, api_secret)
     except Exception as e:
-        return f"Error: {e}"
+        return {'status':'failed', 'content': f"Error: {e}"}
     
     try:
         client_accounts = client.get_accounts()
     except Exception as e:
-        return f"Error: {e.message}"
+        return {'status':'failed', 'content': f"Error: {e.message}"}
 
     data = []
     for each_wallet in client_accounts["data"]:
         wallet_id = each_wallet.id
         transactions = client.get_transactions(wallet_id)
         data.append(transactions)
-    return data
+    return {'status': 'success', 'content': data}
 
