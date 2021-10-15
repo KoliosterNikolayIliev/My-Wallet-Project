0740f54d319701fb0d864434165292eb9df3979c
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-10-15 10:23:16 +0300

    Adding Main Container element

--- campaign/client/src/components/ContainerComponent.js
+++ campaign/client/src/components/ContainerComponent.js
@@ -1,26 +1,21 @@
 import React from "react";
 
-const ContainerComponent = ({
-  imagePath,
-  paragraphOne,
-  paragraphTwo,
-  subtitle,
-}) => {
+const ContainerComponent = ({ imagePath, paragraphOne, subtitle }) => {
   return (
     <article>
-          <div class="col-sm-6">
-            <div class="sub-title">{subtitle}</div>
-            <span class="more-info">
-              <p>{paragraphOne}</p>
-            </span>
-            <div class="start"><a href="#">Get started -- </a>
-              </div>
-          </div>
+      <div class="col-sm-6">
+        <div class="sub-title">{subtitle}</div>
+        <span class="more-info">
+          <p>{paragraphOne}</p>
+        </span>
+        <div class="start">
+          <a href="#">Get started -- </a>
+        </div>
+      </div>
 
-          <div class="col-sm-2">
-            <img src={imagePath} alt="Marketing"></img>
-          </div>
-
+      <div class="col-sm-2">
+        <img src={imagePath} alt="Marketing"></img>
+      </div>
     </article>
   );
 };
