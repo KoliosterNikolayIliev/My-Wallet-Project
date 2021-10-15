678bb77f21e699ac20e230491cd4692ddf5585ee
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-10-15 10:35:01 +0300

    Adding css file and landing page structure

--- campaign/client/src/components/LandingPage.js
+++ campaign/client/src/components/LandingPage.js
@@ -13,35 +13,37 @@ const LandingPage = () => {
 
       <TopBodyComponent />
 
-      <div class="subsc-form">
-        <MailchimpFormContainer />
-      </div>
+      <MailchimpFormContainer />
 
       <MessageComponent />
 
-      <div class="main-content">
-        <ContainerComponent
-          subtitle="Seamless live feed from all your accounts"
-          paragraphOne="No more manual inputs. Connect to your bank, stock brokerage and crypto platforms once and get live updates with the latest state of your portfolio."
-          imagePath="/images/img-2.jpg"
-        />
+      <article>
+        <div class="main-content">
+          <ContainerComponent
+            subtitle="Seamless live feed from all your accounts"
+            paragraphOne="No more manual inputs. Connect to your bank, stock brokerage and crypto platforms once and get live updates with the latest state of your portfolio."
+            imagePath="/images/img-2.jpg"
+          />
 
-        <ContainerComponent
-          subtitle="Secure, Private, Yours"
-          paragraphOne="Trivial will never sell or use your financial data for advertising. Your accounts and data are private, protected and encrypted at rest and in transit."
-          imagePath="/images/img-3.jpg"
-        />
+          <ContainerComponent
+            subtitle="Secure, Private, Yours"
+            paragraphOne="Trivial will never sell or use your financial data for advertising. Your accounts and data are private, protected and encrypted at rest and in transit."
+            imagePath="/images/img-3.jpg"
+          />
 
-        <ContainerComponent
-          subtitle="Unlock the power of your data"
-          paragraphOne="With all your financial data in one place, you can make better decisions to achieve your wealth goals. We will continue bringing features to help you reach financial milestones quicker, safer and smoother."
-          imagePath="/images/img-2.jpg"
-        />
-      </div>
+          <ContainerComponent
+            subtitle="Unlock the power of your data"
+            paragraphOne="With all your financial data in one place, you can make better decisions to achieve your wealth goals. We will continue bringing features to help you reach financial milestones quicker, safer and smoother."
+            imagePath="/images/img-2.jpg"
+          />
+        </div>
+      </article>
 
-      <div>
-        <MailchimpFormContainer />
-      </div>
+      <article>
+        <div class="last-subscribtion">
+          <MailchimpFormContainer />
+        </div>
+      </article>
 
       <FooterComponent />
     </main>
