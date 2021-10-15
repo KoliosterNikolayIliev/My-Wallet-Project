268f762868d5fa2a08cd7d632641756796815ece
Kalin Petrov <kalinpetrovbg@gmail.com>
2021-10-15 09:22:38 +0300

    Adding Header Component final version

--- campaign/client/src/components/HeaderComponent.js
+++ campaign/client/src/components/HeaderComponent.js
@@ -3,46 +3,25 @@ import React from "react";
 const HeaderComponent = () => {
   return (
     <header>
-      <div class="top-navigation">
-
-        <div class="logo">
-        <img src="/images/logo.png">
-      </img>
-        </div>
-
+      <article>
+        <img src="/images/logo.png" alt="logo"></img>
+      </article>
+      <nav>
         <div className="top-nav-menu">
           <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Features</a></li>
-            <li><a href="#">Contact</a></li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Features</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
           </ul>
-
-          <div class="top-nav-btn">
-          Get early access
-          </div>
-
+          <div class="top-nav-btn">Get early access</div>
         </div>
-
-
-      </div>
-
-      <div class="header">
-        <div class="header-text">
-        <h1>Unleash your wealth</h1>
-        <p>
-        New-age wealth builders constantly juggle with multiple investment 
-        platforms. Making sense of the big picture is arduous when it really 
-        shouldn’t be.
-        </p>
-        <p>We bring seamless order to your wealth so you can spend more time growing it.</p>
-        </div>
-
-      <div class="header-image">
-          <img src="/images/img-1.jpg">
-      </img>
-      </div>
-      </div>
-
+      </nav>
     </header>
   );
 };
--- campaign/client/src/components/LandingPage.js
+++ campaign/client/src/components/LandingPage.js
@@ -3,12 +3,15 @@ import MailchimpFormContainer from "./MailChimpFormContainer";
 import ContainerComponent from "./ContainerComponent";
 import FooterComponent from "./FooterComponent";
 import HeaderComponent from "./HeaderComponent";
+import TopBodyComponent from "./TopBodyComponent";
 import MessageComponent from "./MessageComponent";
 
 const LandingPage = () => {
   return (
     <main>
       <HeaderComponent />
+
+      <TopBodyComponent />
 
       <div class="subsc-form">
         <MailchimpFormContainer />
@@ -17,23 +20,23 @@ const LandingPage = () => {
       <MessageComponent />
 
       <div class="main-content">
-      <ContainerComponent
-        subtitle="Seamless live feed from all your accounts"
-        paragraphOne="No more manual inputs. Connect to your bank, stock brokerage and crypto platforms once and get live updates with the latest state of your portfolio."
-        imagePath="/images/img-2.jpg"
-      />
+        <ContainerComponent
+          subtitle="Seamless live feed from all your accounts"
+          paragraphOne="No more manual inputs. Connect to your bank, stock brokerage and crypto platforms once and get live updates with the latest state of your portfolio."
+          imagePath="/images/img-2.jpg"
+        />
 
-      <ContainerComponent
-        subtitle="Secure, Private, Yours"
-        paragraphOne="Trivial will never sell or use your financial data for advertising. Your accounts and data are private, protected and encrypted at rest and in transit."
-        imagePath="/images/img-3.jpg"
-      />
+        <ContainerComponent
+          subtitle="Secure, Private, Yours"
+          paragraphOne="Trivial will never sell or use your financial data for advertising. Your accounts and data are private, protected and encrypted at rest and in transit."
+          imagePath="/images/img-3.jpg"
+        />
 
-      <ContainerComponent
-        subtitle="Unlock the power of your data"
-        paragraphOne="With all your financial data in one place, you can make better decisions to achieve your wealth goals. We will continue bringing features to help you reach financial milestones quicker, safer and smoother."
-        imagePath="/images/img-2.jpg"
-      />
+        <ContainerComponent
+          subtitle="Unlock the power of your data"
+          paragraphOne="With all your financial data in one place, you can make better decisions to achieve your wealth goals. We will continue bringing features to help you reach financial milestones quicker, safer and smoother."
+          imagePath="/images/img-2.jpg"
+        />
       </div>
 
       <div>
