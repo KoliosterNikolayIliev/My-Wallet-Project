7553227adc08ebb4a831250e56b0f77871966702
Sheko1 <martinkypar@gmail.com>
2021-10-19 10:38:30 +0300

    added mock for coinbase transaction

--- assets/utils/coinbase_api.py
+++ assets/utils/coinbase_api.py
@@ -1,3 +1,5 @@
+import os
+
 from coinbase.wallet.client import Client
 
 
@@ -6,32 +8,53 @@ def get_account_balances(api_key, api_secret):
         client = Client(api_key, api_secret)
     except Exception as e:
         return {'status': 'failed', 'content': f"Error: {e}"}
-    
+
     try:
         client_accounts = client.get_accounts()
     except Exception as e:
         return {'status': 'failed', 'content': f"Error: {e.message}"}
-    
+
     data = {}
     try:
         for account in client_accounts["data"]:
             if float(account["balance"].amount) > 0:
-                data[account["id"]] = ({"symbol": account["balance"].currency, "quantity": account["balance"].amount, "value": account["native_balance"]})
+                data[account["id"]] = ({"symbol": account["balance"].currency, "quantity": account["balance"].amount,
+                                        "value": account["native_balance"]})
     except:
         return {'status': 'failed', 'content': f"Error: unknown error"}
     return {'status': 'success', 'content': data}
 
 
 def get_transactions(api_key, api_secret):
+    if os.environ.get('USE_MOCK') == 'True':
+        return {
+            "status": "success",
+            "content": {
+                "DOGE Wallet": {
+                    "6f26d0a9-15c3-58b3-8c1e-cd974fdcdebf": {
+                        "amount": "-200.00000000",
+                        "currency": "DOGE"
+                    },
+                    "ae203706-4a5c-5524-b804-723a95800e0e": {
+                        "amount": "-9995.00000000",
+                        "currency": "DOGE"
+                    },
+                    "a4adf54c-f3da-584f-8f09-5a4de77fcbb4": {
+                        "amount": "19995.00000000",
+                        "currency": "DOGE"
+                    }
+                }
+            }
+        }
     try:
         client = Client(api_key, api_secret)
     except Exception as e:
-        return {'status':'failed', 'content': f"Error: {e}"}
-    
+        return {'status': 'failed', 'content': f"Error: {e}"}
+
     try:
         client_accounts = client.get_accounts()
     except Exception as e:
-        return {'status':'failed', 'content': f"Error: {e.message}"}
+        return {'status': 'failed', 'content': f"Error: {e.message}"}
 
     data = {}
     try:
@@ -45,10 +68,9 @@ def get_transactions(api_key, api_secret):
                     # return only the completed transactions
                     if transaction["status"] == "completed":
                         transaction_data[transaction["id"]] = transaction['amount']
-                
+
                 data[wallet.name] = transaction_data
     except:
         return {'status': 'failed', 'content': f"Error: unknown error"}
 
     return {'status': 'success', 'content': data}
-
