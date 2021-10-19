402984e6b3e20d42acce6700f412eed39fa27601
Sheko1 <martinkypar@gmail.com>
2021-10-19 19:34:04 +0300

    fixed mock for binance holdings

--- assets/utils/binance_api.py
+++ assets/utils/binance_api.py
@@ -9,6 +9,9 @@ def validate_api_key_and_api_secret(api_key, api_secret):
         # return false bool to say the validation failed and the error message
         return False, {'status': 'failed', 'content': 'Error: API key or API secret was not provided'}
 
+    if os.environ.get('USE_MOCK') == 'True':
+        return True,
+
     client = Spot(api_key, api_secret)
 
     # try to make a request and if it fails it means the api key or api secret is invalid
@@ -24,33 +27,6 @@ def validate_api_key_and_api_secret(api_key, api_secret):
 
 
 def get_balances(api_key, api_secret):
-    if os.environ.get('USE_MOCK') == 'True':
-        return {
-            "status": "success",
-            "content": {
-                "19713826504519158012514033432436410360": {
-                    "symbol": "BTC",
-                    "quantity": "0.00069389"
-                },
-                "312449119748704017695817878114511750902": {
-                    "symbol": "ETH",
-                    "quantity": "0.00590000"
-                },
-                "168431868865493663656248984534282715018": {
-                    "symbol": "BNB",
-                    "quantity": "0.00238492"
-                },
-                "64884506774569230777743708680327731544": {
-                    "symbol": "ADA",
-                    "quantity": "0.09267368"
-                },
-                "334804415272661327165161730551465081020": {
-                    "symbol": "SOL",
-                    "quantity": "0.16079883"
-                }
-            }
-        }
-
     # check validation of api key and api secret
     validation = validate_api_key_and_api_secret(api_key, api_secret)
 
@@ -58,9 +34,18 @@ def get_balances(api_key, api_secret):
         # if validation failed we return the error message
         return validation[1]
 
-    client = Spot(api_key, api_secret)
-    # gets all balances assets
-    balances = client.account().get('balances')
+    if os.environ.get('USE_MOCK') != 'True':
+        client = Spot(api_key, api_secret)
+        # gets all balances assets
+        balances = client.account().get('balances')
+
+    else:
+        balances = [{'asset': 'BTC', 'free': '0.00069389', 'locked': '0.00000000'},
+                    {'asset': 'LTC', 'free': '0.00000000', 'locked': '0.00000000'},
+                    {'asset': 'ETH', 'free': '0.00590000', 'locked': '0.00000000'},
+                    {'asset': 'NEO', 'free': '0.00000000', 'locked': '0.00000000'},
+                    {'asset': 'BNB', 'free': '0.00238492', 'locked': '0.00000000'},
+                    ]
 
     data = {}
 
