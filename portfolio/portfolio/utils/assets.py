49c789873adc9af9b9774e0852e58008a501f232
Dimo Dimchev <ddimchev7@gmail.com>
2021-10-20 19:44:55 +0300

    #118 Moved logic for getting balances and holdings into one endpoint

--- portfolio/portfolio/utils/assets.py
+++ portfolio/portfolio/utils/assets.py
@@ -3,7 +3,7 @@ import requests, os
 URL = os.environ.get("ASSETS_URL")
 
 # fetch all balances using Assets
-def get_balances(headers):
+async def get_balances(headers):
     res = requests.get(URL + "balances/", headers=headers)
     return res.json()
 
@@ -14,6 +14,6 @@ def get_transactions(headers):
     return res.json()
 
 # fetch all holdings using Assets
-def get_holdings(headers):
+async def get_holdings(headers):
     res = requests.get(URL + "holdings/", headers=headers)
     return res.json()
