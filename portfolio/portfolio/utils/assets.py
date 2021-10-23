0ad418493be500680a0f8779548f80dbc3b35eb8
Dimo Dimchev <ddimchev7@gmail.com>
2021-10-23 17:02:16 +0300

    #124 Updated enpoints

--- portfolio/portfolio/utils/assets.py
+++ portfolio/portfolio/utils/assets.py
@@ -5,17 +5,17 @@ URL = os.environ.get("ASSETS_URL")
 
 # fetch all balances using Assets
 async def get_balances(headers, session):
-    async with session.get(URL + "balances/", headers=headers, ssl=False) as res:
+    async with session.get(URL + "balances", headers=headers, ssl=False) as res:
         return await res.json()
 
 
 
 # fetch all transactions using Assets
 def get_transactions(headers):
-    res = requests.get(URL + "transactions/", headers=headers)
+    res = requests.get(URL + "transactions", headers=headers)
     return res.json()
 
 # fetch all holdings using Assets
 async def get_holdings(headers, session):
-    async with session.get(URL + "holdings/", headers=headers, ssl=False) as res:
+    async with session.get(URL + "holdings", headers=headers, ssl=False) as res:
         return await res.json()
