dc51268df88756537b29b966528aa96496cc26a2
Dimo Dimchev <ddimchev7@gmail.com>
2021-10-26 21:09:34 +0300

    #131 Changed to production credentials

--- campaign/client/src/components/EarlyAccessForm.js
+++ campaign/client/src/components/EarlyAccessForm.js
@@ -1,188 +1,196 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import ReactModal from "react-modal";
 import QuizComponent from "./QuizComponent";
-import cross from "../images/cross.svg"
+import cross from "../images/cross.svg";
 import CongratulateComponent from "./CongratulateComponent";
-
 
 // custom form to use with MailChimpFormContainer
 const EarlyAccessForm = ({
-                             counter,
-                             setCounter,
-                             status,
-                             message,
-                             onSubmitted,
-                         }) => {
-    const [email, setEmail] = useState("");
-    const [modalIsOpen, setIsOpen] = useState(false);
-    const [quizState, setQuizState] = useState(1);
-    const [answerSheet, setAnswerSheet] = useState([]);
+  counter,
+  setCounter,
+  status,
+  message,
+  onSubmitted,
+}) => {
+  const [email, setEmail] = useState("");
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [quizState, setQuizState] = useState(1);
+  const [answerSheet, setAnswerSheet] = useState([]);
 
-    const sendData = async () => {
-        let data = {
-            author: email,
-            quiz: answerSheet,
-        };
-        await fetch("http://localhost:5000/save-form-data", {
-            method: "POST",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(data),
-        });
+  const sendData = async () => {
+    let data = {
+      author: email,
+      quiz: answerSheet,
     };
+    await fetch("http://localhost:5000/save-form-data", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+  };
 
-    const openModal = () => {
-        setIsOpen(true);
-    };
+  const openModal = () => {
+    setIsOpen(true);
+  };
 
-    const closeModal = () => {
-        setIsOpen(false);
-        setQuizState(1)
+  const closeModal = () => {
+    setIsOpen(false);
+    setQuizState(1);
+  };
 
-    };
+  // send answers to database
+  const sendAnswers = () => {
+    setQuizState(1);
+    setAnswerSheet([]);
+    sendData();
+    clearFields();
+    // closeModal();
+    setQuizState(4);
+  };
+  const statusMessage = {
+    success: "Thanks for subscribing!",
+    error: "Invalid email",
+    sending: "Sending...",
+  };
 
-    // send answers to database
-    const sendAnswers = () => {
-        setQuizState(1);
-        setAnswerSheet([]);
-        sendData();
-        clearFields();
-        // closeModal();
-        setQuizState(4);
-    };
-    const statusMessage = {
-        "success":"Thanks for subscribing!",
-        "error": "Invalid email",
-        "sending":"Sending..."
-    };
+  useEffect(() => {
+    if (status === "success") {
+      openModal();
+    }
+  }, [status]);
 
-    useEffect(() => {
-        if (status === "success") {
-            openModal();
-        }
-    }, [status]);
+  const clearFields = () => {
+    setEmail("");
+  };
 
-    const clearFields = () => {
-        setEmail("");
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    email &&
+      email.indexOf("@") > -1 &&
+      onSubmitted({
+        EMAIL: email,
+      });
+    setCounter(counter + 1);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        email &&
-        email.indexOf("@") > -1 &&
-        onSubmitted({
-            EMAIL: email,
-        });
-        setCounter(counter + 1);
-    };
+  return (
+    <article>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="email"
+          value={email}
+          placeholder={
+            status === null ? "Your email..." : statusMessage[status]
+          }
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input type="submit" value="Get early access" />
+      </form>
+      <p style={{ display: "block" }}>
+        {status === "sending" && <span>Sending...</span>}
+        {status === "error" && (
+          <span style={{ color: "red" }}>Invalid email</span>
+        )}
+        {/*{status === "success" && <span>Thanks for subscribing!</span>}*/}
+      </p>
 
-    return (
-        <article>
-            <form onSubmit={(e) => handleSubmit(e)}>
+      <ReactModal
+        isOpen={modalIsOpen}
+        // onRequestClose={closeModal}
+        className="Modal"
+        overlayClassName="Overlay"
+        contentLabel="Example Modal"
+        ariaHideApp={false}
+      >
+        {quizState !== 4 ? (
+          <div className="heading-text">
+            <span className="modalHeading">Thank you for subscribing!</span>
+            <button className="modal-close-button" onClick={closeModal}>
+              <img alt={"missing"} src={cross} />
+            </button>
 
-                <input
-                       type="email"
-                       value={email}
-                       placeholder={status===null?"Your email...":statusMessage[status]}
-                       onChange={(e) => setEmail(e.target.value)}
-                />
-                <input type="submit" value="Get early access"/>
-
-            </form>
-            <p style={{display:"block"}}>
-                {status === "sending" && <span>Sending...</span>}
-                {status === "error" && <span style={{color:"red"}}>Invalid email</span>}
-                {/*{status === "success" && <span>Thanks for subscribing!</span>}*/}
+            <p className="sub-heading">
+              We would really appreciate it if you answered a few questions that
+              can help us with the development of the platform.
             </p>
-
-            <ReactModal
-                isOpen={modalIsOpen}
-                // onRequestClose={closeModal}
-                className="Modal"
-                overlayClassName="Overlay"
-                contentLabel="Example Modal"
-                ariaHideApp={false}
-            >
-                {quizState !== 4 ?
-                    <div className="heading-text">
-                        <span className="modalHeading">Thank you for subscribing!</span>
-                        <button className="modal-close-button" onClick={closeModal}><img alt={'missing'} src={cross}/>
-                        </button>
-
-                        <p className="sub-heading">
-                            We would really appreciate it if you answered a few questions that can
-                            help us with the development of the platform.
-                        </p>
-                        <div className="modal-border-container">
-                            <span className="purple-modal-border"/>
-                            <span className={quizState > 1 ? "purple-modal-border" : "grey-modal-border"}/>
-                            <span className={quizState > 2 ? "purple-modal-border" : "grey-modal-border"}/>
-                            <span className="grey-modal-border"/>
-                        </div>
-                    </div> :
-                    <button className="modal-close-button" onClick={closeModal}><img alt={'missing'} src={cross}/>
-                    </button>
+            <div className="modal-border-container">
+              <span className="purple-modal-border" />
+              <span
+                className={
+                  quizState > 1 ? "purple-modal-border" : "grey-modal-border"
                 }
+              />
+              <span
+                className={
+                  quizState > 2 ? "purple-modal-border" : "grey-modal-border"
+                }
+              />
+              <span className="grey-modal-border" />
+            </div>
+          </div>
+        ) : (
+          <button className="modal-close-button" onClick={closeModal}>
+            <img alt={"missing"} src={cross} />
+          </button>
+        )}
 
-                {quizState === 1 && (
-                    <QuizComponent
-                        questionType={"closed"}
-                        quizState={quizState}
-                        setQuizState={setQuizState}
-                        answerSheet={answerSheet}
-                        setAnswerSheet={setAnswerSheet}
-                        submitFunction={sendAnswers}
-                        question={
-                            "What do you hope Trivial will help you with? (Select all that apply)"
-                        }
-                        answer1={"Seeing all my investment accounts in one place"}
-                        answer2={"Setting clear and meaningful financial goals"}
-                        answer3={"Making better financial decisions"}
-                        answer4={"Getting my taxes in order"}
-                    />
-                )}
+        {quizState === 1 && (
+          <QuizComponent
+            questionType={"closed"}
+            quizState={quizState}
+            setQuizState={setQuizState}
+            answerSheet={answerSheet}
+            setAnswerSheet={setAnswerSheet}
+            submitFunction={sendAnswers}
+            question={
+              "What do you hope Trivial will help you with? (Select all that apply)"
+            }
+            answer1={"Seeing all my investment accounts in one place"}
+            answer2={"Setting clear and meaningful financial goals"}
+            answer3={"Making better financial decisions"}
+            answer4={"Getting my taxes in order"}
+          />
+        )}
 
-                {quizState === 2 && (
-                    <QuizComponent
-                        questionType={"closed"}
-                        numberOfAnswers={1}
-                        quizState={quizState}
-                        setQuizState={setQuizState}
-                        answerSheet={answerSheet}
-                        setAnswerSheet={setAnswerSheet}
-                        submitFunction={sendAnswers}
-                        question={
-                            "What do you use to solve this problem now? (Select only one option)"
-                        }
-                        answer1={"Nothing"}
-                        answer2={"Spreadsheets"}
-                        answer3={"I've set up my own API"}
-                        answer4={"Financial advisor"}
-                    />
-                )}
+        {quizState === 2 && (
+          <QuizComponent
+            questionType={"closed"}
+            numberOfAnswers={1}
+            quizState={quizState}
+            setQuizState={setQuizState}
+            answerSheet={answerSheet}
+            setAnswerSheet={setAnswerSheet}
+            submitFunction={sendAnswers}
+            question={
+              "What do you use to solve this problem now? (Select only one option)"
+            }
+            answer1={"Nothing"}
+            answer2={"Spreadsheets"}
+            answer3={"I've set up my own API"}
+            answer4={"Financial advisor"}
+          />
+        )}
 
-                {quizState === 3 && (
-                    <QuizComponent
-                        questionType={"open"}
-                        quizState={quizState}
-                        setQuizState={setQuizState}
-                        answerSheet={answerSheet}
-                        setAnswerSheet={setAnswerSheet}
-                        submitFunction={sendAnswers}
-                        question={
-                            "Please list all of the investment platforms you use today?"
-                        }
-                    />
-                )}
-                {quizState === 4 && (
-                    <CongratulateComponent/>
-                )}
+        {quizState === 3 && (
+          <QuizComponent
+            questionType={"open"}
+            quizState={quizState}
+            setQuizState={setQuizState}
+            answerSheet={answerSheet}
+            setAnswerSheet={setAnswerSheet}
+            submitFunction={sendAnswers}
+            question={
+              "Please list all of the investment platforms you use today?"
+            }
+          />
+        )}
+        {quizState === 4 && <CongratulateComponent />}
 
-                {/* <button onClick={sendAnswers}>Submit answers</button>
+        {/* <button onClick={sendAnswers}>Submit answers</button>
         <button onClick={closeModal}>I don't want to answer</button> */}
-            </ReactModal>
-        </article>
-    );
+      </ReactModal>
+    </article>
+  );
 };
 
 export default EarlyAccessForm;
-
--- campaign/server/server.js
+++ campaign/server/server.js
@@ -23,7 +23,6 @@ const Questionnaire = require("./models/questionnaire");
 
 const getLists = async () => {
   const lists = await mailchimp.lists.getAllLists();
-  console.log(lists["lists"][0]["stats"]);
   return lists["lists"][0]["stats"]["member_count_since_send"];
 };
 
