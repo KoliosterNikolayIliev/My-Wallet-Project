638445c6555e3eb65aa1b72c4043b03255d1f257
Sheko1 <martinkypar@gmail.com>
2021-10-28 23:33:03 +0300

    added onhover effect for assets that have transactions

--- client/src/components/Other/BalanceComponent.js
+++ client/src/components/Other/BalanceComponent.js
@@ -3,7 +3,7 @@ import React from "react";
 const BalanceComponent = ({ providerName, amount, currency, provider, account, getTransactionsFunc }) => {
   return (
     <div>
-      <p onClick={() => getTransactionsFunc(provider, account)}>
+      <p onClick={() => getTransactionsFunc(provider, account)} className='has-transactions'>
         {providerName}: {amount} {currency}
       </p>
     </div>
