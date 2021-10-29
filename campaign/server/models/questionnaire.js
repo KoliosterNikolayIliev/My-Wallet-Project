65fca734c7c4fba6ac7390f6b4fcaa264fe8fa7c
Dimo Dimchev <ddimchev7@gmail.com>
2021-10-29 11:04:29 +0300

    #146 Removed Node.js server

--- campaign/server/models/questionnaire.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-// create questionaire schema
-const questionnaireSchema = new mongoose.Schema({
-  author: String,
-  quiz: Object,
-});
-
-module.exports = mongoose.model("Questionnaire", questionnaireSchema);
