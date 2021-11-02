b5da84bf46be6d8ef0aeb717d2ed3639d5ed453f
Nikolay Iliev <65191727+KoliosterNikolayIliev@users.noreply.github.com>
2021-11-02 16:21:00 +0200

    #176 Fix tests

--- server/account/account/authentication/models.py
+++ server/account/account/authentication/models.py
@@ -18,27 +18,9 @@ class UserProfile(models.Model):
     # Coinbase access
     coinbase_api_secret = EncryptedCharField(max_length=200, blank=True)
     coinbase_api_key = EncryptedCharField(max_length=200, blank=True)
- 
+
 
 class NordigenRequisition(models.Model):
-    """
-    Trigger function for Mongo DB:
-    Needed for creating ID for the model
-    how to setup trigger in mongo DB Atlas: https://www.mongodb.com/basics/mongodb-auto-increment
-    Trigger name: nordigenRequisitionId
-
-    exports = async function(changeEvent) {
-        var docId = changeEvent.fullDocument._id;
-
-        const countercollection = context.services.get("3vial").db(changeEvent.ns.db).collection("counters");
-        const authentication_nordigenrequisition = context.services.get("3vial").db(changeEvent.ns.db).collection(changeEvent.ns.coll);
-
-        var counter = await countercollection.findOneAndUpdate({_id: changeEvent.ns },{ $inc: { seq_value: 1 }}, { returnNewDocument: true, upsert : true});
-        var updateRes = await authentication_nordigenrequisition.updateOne({_id : docId},{ $set : {id : counter.seq_value}});
-
-        console.log(`Updated ${JSON.stringify(changeEvent.ns)} with counter ${counter.seq_value} result : ${JSON.stringify(updateRes)}`);
-        };
-    """
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     institution_id = models.CharField(unique=True, max_length=100)
     requisition_id = EncryptedCharField(max_length=100)
