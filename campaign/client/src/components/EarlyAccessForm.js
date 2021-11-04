980ef1293dd709a53f20b88b072cddc9604281a9
Nikolay Iliev <65191727+KoliosterNikolayIliev@users.noreply.github.com>
2021-11-04 16:22:02 +0200

    #152 input field to button

--- campaign/client/src/components/EarlyAccessForm.js
+++ campaign/client/src/components/EarlyAccessForm.js
@@ -84,7 +84,7 @@ const EarlyAccessForm = ({
           }
           onChange={(e) => setEmail(e.target.value)}
         />
-        <input className="get-early-access-btn" type="submit" value="Get early access" />
+        <button className="get-early-access-btn" type="submit">"Get early access"</button>
       </form>
       <p style={{ display: "block" }}>
         {status === "sending" && <span>Sending...</span>}
