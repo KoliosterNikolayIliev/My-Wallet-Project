d79b8df7f17d3d75002c5a8e7172e10a44b76ae7
Sheko1 <martinkypar@gmail.com>
2021-11-10 12:34:05 +0200

    if user_id exist it will update old object instead creating new one

--- server/portfolio/portfolio/utils/cache_assets.py
+++ server/portfolio/portfolio/utils/cache_assets.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 import pymongo
 
 client = pymongo.MongoClient(os.environ.get('DB_HOST'))
@@ -7,10 +7,12 @@ db = client.get_database('portfolio')
 
 
 def cache_assets(assets, user_id):
-    current_datetime = datetime.utcnow()
-    data = {
-        'user_id': user_id,
-        'date': current_datetime,
-        'content': assets,
-    }
-    db.assets.insert_one(data)
+    result = db.assets.update_one({'user_id': user_id}, {'$set': {'date': datetime.utcnow(), 'content': assets}})
+
+    if result.modified_count == 0:
+        data = {
+            'user_id': user_id,
+            'date': datetime.utcnow(),
+            'content': assets,
+        }
+        db.assets.insert_one(data)
