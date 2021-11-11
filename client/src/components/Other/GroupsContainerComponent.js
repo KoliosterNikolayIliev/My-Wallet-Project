3c93cc890543bc629c22c7600d60d04323796dc4
Dimo Dimchev <ddimchev7@gmail.com>
2021-11-11 20:20:10 +0200

    #224 Nest yodlee assets if there is more than one acc

--- client/src/components/Other/GroupsContainerComponent.js
+++ client/src/components/Other/GroupsContainerComponent.js
@@ -19,34 +19,59 @@ const GroupsContainerComponent = ({
               {value.total.toFixed(2)} {baseSymbol}
             </h3>
             <ul>
-              {value.accounts.map((account) => {
-                return (
-                  <GroupComponent
-                    source={source[0].toUpperCase() + source.slice(1)}
-                    baseSymbol={baseSymbol}
-                    provider={account.provider}
-                    account={account}
-                    type={account.data.accountType}
-                    getTransactionsFunc={getTransactionsFunc}
-                  />
-                );
-              })}
-              {value.accounts.map((account) => {
-                if (account.holdings) {
-                  if (account.holdings.length > 0) {
-                    return account.holdings.map((holding) => {
-                      return (
-                        <HoldingComponent
-                          data={holding}
-                          baseSymbol={baseSymbol}
-                        />
-                      );
-                    });
+              {(value.accounts.length > 1 || source === "coinbase") &&
+                value.accounts.map((account) => {
+                  return (
+                    <GroupComponent
+                      source={source[0].toUpperCase() + source.slice(1)}
+                      baseSymbol={baseSymbol}
+                      provider={account.provider}
+                      account={account}
+                      type={account.data.accountType}
+                      getTransactionsFunc={getTransactionsFunc}
+                    />
+                  );
+                })}
+              {value.accounts.length <= 1 &&
+                value.accounts.map((account) => {
+                  if (account.holdings) {
+                    if (account.holdings.length > 0) {
+                      return account.holdings.map((holding) => {
+                        return (
+                          <HoldingComponent
+                            nest={false}
+                            data={holding}
+                            baseSymbol={baseSymbol}
+                          />
+                        );
+                      });
+                    }
+                    return null;
                   }
                   return null;
-                }
-                return null;
-              })}
+                })}
+              {value.accounts.length > 1 &&
+                value.accounts.map((account) => {
+                  if (account.holdings) {
+                    if (account.holdings.length > 0) {
+                      return (
+                        <ul>
+                          {account.holdings.map((holding) => {
+                            return (
+                              <HoldingComponent
+                                nest={true}
+                                data={holding}
+                                baseSymbol={baseSymbol}
+                              />
+                            );
+                          })}
+                        </ul>
+                      );
+                    }
+                    return null;
+                  }
+                  return null;
+                })}
             </ul>
           </div>
         );
