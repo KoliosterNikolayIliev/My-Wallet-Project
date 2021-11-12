ec9634a807c31ef8863bfeea1a4fbd130318ba2d
Nikolay Iliev <65191727+KoliosterNikolayIliev@users.noreply.github.com>
2021-11-12 14:45:16 +0200

    #230 Refactor and bugs fixed

--- server/monitor/monitor_app/utils.py
+++ server/monitor/monitor_app/utils.py
@@ -35,7 +35,7 @@ def get_assets():
     return 15000
 
 
-def get_user_totp_device(self, user, confirmed=None):
+def get_user_totp_device(user, confirmed=None):
     devices = devices_for_user(user, confirmed=confirmed)
     for device in devices:
         if isinstance(device, TOTPDevice):
