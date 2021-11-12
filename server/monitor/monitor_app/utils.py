53bc8e2792c76982692cd9c5fda4a7198d54f0cc
Sheko1 <martinkypar@gmail.com>
2021-11-12 12:14:10 +0200

    refactored db connection

--- server/monitor/monitor_app/utils.py
+++ server/monitor/monitor_app/utils.py
@@ -4,29 +4,11 @@ from django_otp.plugins.otp_totp.models import TOTPDevice
 from pymongo import MongoClient
 
 
-def get_db_handle(db_name, host, port, username, password):
-    client = MongoClient(
-        host=host,
-        port=int(port),
-        username=username,
-        password=password
-    )
-    return client[db_name]
+client = MongoClient(host=os.environ.get('DB_HOST'))
 
 
 def get_number_of_users():
-    db_name = '3vial'
-    db_host = os.environ.get('DB_HOST')
-    db_port = 27017
-    db_username = os.environ.get('DB_USERNAME').strip()
-    db_password = os.environ.get('DB_PASSWORD').strip()
-
-    db = get_db_handle(
-        db_name,
-        db_host,
-        db_port,
-        db_username,
-        db_password)
+    db = client['3vial']
     collection = db['authentication_userprofile']
     return collection.estimated_document_count()
 
