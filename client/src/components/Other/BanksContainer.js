979c3255fa1f7647946064c0279fc8ffec7c60e5
magardich <martinkypar@gmail.com>
2021-11-25 01:01:36 +0200

    added way to display banks

--- client/src/components/Other/BanksContainer.js
+++ client/src/components/Other/BanksContainer.js
@@ -1,8 +1,14 @@
-import { useAuth0 } from "@auth0/auth0-react";
-import { linkAccount } from "../../utils/nordigen";
+import {useAuth0} from "@auth0/auth0-react";
+import {linkAccount} from "../../utils/nordigen";
+import {IconButton, ImageList, ImageListItem, ImageListItemBar, ListSubheader} from "@mui/material";
+import {Fragment} from "react";
 
-export const BanksContainer = ({ data }) => {
-  const { getAccessTokenSilently } = useAuth0();
+function InfoIcon() {
+  return null;
+}
+
+export const BanksContainer = ({data}) => {
+  const {getAccessTokenSilently} = useAuth0();
 
   const addBankAccount = async (id) => {
     const token = await getAccessTokenSilently();
@@ -12,20 +18,22 @@ export const BanksContainer = ({ data }) => {
   };
 
   if (data === []) return null;
-
   return (
-    <div>
-      <div className="banks-container">
-        {data.map((bank) => {
-          return (
-            <div>
-              <p onClick={() => addBankAccount(bank.id)}>{bank.name}</p>
-              <img src={bank.logo} width={100} />
-            </div>
-          );
-        })}
-      </div>
-    </div>
+    <ImageList sx={{ width: 500, height: 450 }} cols={3}>
+      {data.map((item) => (
+        <ImageListItem key={item.logo}>
+          <img
+            src={`${item.logo}?w=248&fit=crop&auto=format`}
+            srcSet={`${item.logo}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            alt={item.name}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            title={item.name}
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
   );
 };
 
