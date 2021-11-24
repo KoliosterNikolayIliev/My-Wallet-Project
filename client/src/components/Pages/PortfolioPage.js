07ee83fa4cddb468468e08805e4b508e27ef62e6
Dimo Dimchev <ddimchev7@gmail.com>
2021-11-24 20:51:04 +0200

    #258 Added routing between all of the pages

---
+++ client/src/components/Pages/PortfolioPage.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Redirect } from "react-router";
+
+import "../../styles/dashboard.scss";
+import "../../styles/main_content.scss";
+import Loader from "../Other/LoaderComponent";
+
+// Dashboard page to be filled in with user account data
+const PortfolioPage = () => {
+  const { isAuthenticated, user, loading } = useAuth0();
+  //   Return this if Auth0 is still loading. Can be replaced with an animation in the future
+
+  if (loading) {
+    return Loader();
+  }
+
+  return (
+    isAuthenticated && (
+      <div>
+        <h2>This is the portfolio page</h2>
+      </div>
+    )
+  );
+};
+
+export default PortfolioPage;
