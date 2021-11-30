265e836f3f0e224cf38ce07e3ac83335dc9ea3e1
magardich <martinkypar@gmail.com>
2021-11-30 17:04:14 +0200

    added loader for yodlee

--- client/src/components/Other/AddNewSourceModal.js
+++ client/src/components/Other/AddNewSourceModal.js
@@ -4,9 +4,9 @@ import CountriesDropDownList from "./CountriesDropDownList";
 import AddYodleeComponent from "./AddYodleeComponent";
 import AddCryptoSource from "./AddCryptoSource";
 import '../../styles/add-source-modal.scss'
+import Loader from "./LoaderComponent";
 
 const AddNewSourceModal = ({openModal, closeModalFunc, source}) => {
-
   const [content, setContent] = useState()
 
   const selectProvider = (selected) => {
@@ -46,10 +46,11 @@ const AddNewSourceModal = ({openModal, closeModalFunc, source}) => {
     setContent()
     closeModalFunc()
   }
-
+  console.log(sources.loading)
   return (
     <Modal open={openModal} onClose={handleClose}>
       <Box className='modal-box'>
+        {sources.loading === true && <Loader />}
         {content ? content : sources}
       </Box>
     </Modal>
