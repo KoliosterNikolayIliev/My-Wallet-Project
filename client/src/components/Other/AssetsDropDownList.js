1a6aa5adb679f8a009f81642945dbf9550894e68
Dimo Dimchev <ddimchev7@gmail.com>
2021-12-02 09:45:36 +0200

    #282 Added cache reset to custom assets

--- client/src/components/Other/AssetsDropDownList.js
+++ client/src/components/Other/AssetsDropDownList.js
@@ -1,21 +1,21 @@
-import {useEffect, useState} from "react";
-import {useAuth0} from "@auth0/auth0-react";
-import {DropDownList} from "@progress/kendo-react-dropdowns";
-import {createCustomAsset} from "../../utils/portfolio";
-import {MenuItem, Select} from "@mui/material";
-import "../../styles/dashboard.scss"
+import { useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+import { DropDownList } from "@progress/kendo-react-dropdowns";
+import { createCustomAsset } from "../../utils/portfolio";
+import { MenuItem, Select } from "@mui/material";
+import "../../styles/dashboard.scss";
 import Loader from "./LoaderComponent";
 
 const assetTypes = ["crypto", "stock"];
 
 export const AssetsDropDownList = () => {
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
   const [categories, setCategories] = useState([]);
   const [symbol, setSymbol] = useState("");
   const [amount, setAmount] = useState(0);
   const [assetType, setAssetType] = useState("");
 
-  const {getAccessTokenSilently} = useAuth0();
+  const { getAccessTokenSilently } = useAuth0();
 
   useEffect(() => {
     assetTypes.forEach((type) => {
@@ -24,47 +24,55 @@ export const AssetsDropDownList = () => {
   }, []);
 
   const addAsset = async (e, type, symbol, amount) => {
-    setLoading(true)
+    setLoading(true);
     e.preventDefault();
     const token = await getAccessTokenSilently();
     const newAsset = await createCustomAsset(token, type, symbol, amount);
-    setLoading(false)
-    window.location.reload()
+    setLoading(false);
+    window.location.reload();
   };
 
   return (
     <div>
-      {loading ? <Loader/> : (
+      {loading ? (
+        <Loader />
+      ) : (
         <section className="k-my-8">
           <form
             className="form-div"
-            onSubmit={(e) => addAsset(e, assetType, symbol, amount)}
+            onSubmit={(e) => {
+              addAsset(e, assetType, symbol, amount);
+              window.sessionStorage.clear();
+            }}
           >
             <label className="k-label k-mb-3">Asset Type</label>
             <Select
-              style={{width: '50%'}}
+              style={{ width: "50%" }}
               required={true}
               onChange={(e) => setAssetType(e.target.value)}
             >
-              {categories.map(el => <MenuItem value={el}>{el}</MenuItem>)}
+              {categories.map((el) => (
+                <MenuItem value={el}>{el}</MenuItem>
+              ))}
             </Select>
             <input
-              className='form-input'
+              className="form-input"
               type="text"
               placeholder="e.g: BTC"
               onChange={(e) => setSymbol(e.target.value)}
             />
             <input
-              className='form-input'
+              className="form-input"
               type="float"
               placeholder="e.g: 0.01"
               onChange={(e) => setAmount(e.target.value)}
             />
-            <button className='button-main-small'>Add asset</button>
+            <button className="button-main-small">Add asset</button>
           </form>
-        </section>)}
-        </div>
-        );
-      };
+        </section>
+      )}
+    </div>
+  );
+};
 
-      export default AssetsDropDownList;
+export default AssetsDropDownList;
