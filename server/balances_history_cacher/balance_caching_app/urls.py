5868ec7dae7cf87e0360fe85a70f99a3c0444ea3
Nikolay Iliev <65191727+KoliosterNikolayIliev@users.noreply.github.com>
2021-12-06 00:44:50 +0200

    Update urls.py

--- server/balances_history_cacher/balance_caching_app/urls.py
+++ server/balances_history_cacher/balance_caching_app/urls.py
@@ -6,7 +6,7 @@ from balance_caching_app.mock_data_view import mock_data_view
 from balance_caching_app.views import CreateBalance
 
 mock = os.environ.get('USE_BALANCE_CACHE_MOCK')
-if mock:
+if mock == 'True':
     urlpatterns = (
         path('add/', mock_data_view),
     )
