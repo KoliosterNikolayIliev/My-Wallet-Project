91cd3debf0a85c980827e9e5f3e5d31345c112ed
Dimo Dimchev <ddimchev7@gmail.com>
2021-12-06 21:47:36 +0200

    #293 Styled the source groups comparisons

--- client/src/components/Other/GroupsContainerComponent.js
+++ client/src/components/Other/GroupsContainerComponent.js
@@ -3,6 +3,9 @@ import GroupComponent from "./GroupComponent";
 import HoldingComponent from "./HoldingComponent";
 import ExpandButton from "./ExpandButton";
 
+import { useRecoilState } from "recoil";
+import { balanceHistoryAtom } from "../../recoil";
+
 const GroupsContainerComponent = ({
   data,
   total,
@@ -11,17 +14,88 @@ const GroupsContainerComponent = ({
   user,
 }) => {
   let source;
+  const [balanceHistory, setBalanceHistory] =
+    useRecoilState(balanceHistoryAtom);
+
+  if (balanceHistory === "" || !balanceHistory) {
+    if (window.sessionStorage.getItem("balanceHistory")) {
+      setBalanceHistory(
+        JSON.parse(window.sessionStorage.getItem("balanceHistory"))
+      );
+    }
+  }
+
+  const source_balances_history =
+    balanceHistory["balances"][0]["source_balances_history"];
+
   return (
     <div className="data-source-container">
       {Object.entries(data).map(([key, value]) => {
         source = key;
+        let source_previous_balance;
+        let percentage;
+
+        source_balances_history.map((item) => {
+          if (item.provider === source) {
+            source_previous_balance = item.value;
+            percentage = (source_previous_balance / value.total) * 100;
+            if (percentage > 100) {
+              percentage -= 100;
+            } else {
+              percentage = 100 - percentage;
+            }
+          }
+        });
+
         return (
           <div className="data-source">
             <div className="data-source-header">
-              <p>{source[0].toUpperCase() + source.slice(1)} </p>
+              <p className="data-source-name">
+                {source !== "custom_assets"
+                  ? source[0].toUpperCase() + source.slice(1)
+                  : "Assets"}{" "}
+              </p>
               <p className="data-source-total">
                 {value.total.toFixed(2)} {baseSymbol}
               </p>
+              {value.total !== source_previous_balance ? (
+                value.total < source_previous_balance ? (
+                  <p className="data-source-change positive">
+                    <svg
+                      width="16"
+                      height="11"
+                      viewBox="0 0 16 11"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M 5.9588 0.1946 C 5.8565 0.0714 5.7039 0 5.5428 0 C 5.3817 0 5.2291 0.0714 5.1268 0.1946 L 0.4468 6.3758 C 0.3286 6.5319 0.3096 6.7407 0.3977 6.9152 C 0.4858 7.0897 0.6658 7.2 0.8628 7.2 H 10.2228 C 10.4198 7.2 10.5998 7.0897 10.6879 6.9152 C 10.776 6.7407 10.757 6.5319 10.6388 6.3758 L 5.9588 0.1946 Z"
+                        fill="#4FBF67"
+                      />
+                    </svg>
+                    {percentage}%
+                  </p>
+                ) : (
+                  <p className="data-source-change negative">
+                    <svg
+                      width="16"
+                      height="11"
+                      viewBox="0 0 16 11"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      style={{ transform: "rotate(180deg)" }}
+                    >
+                      <path
+                        d="M 5.9588 0.1946 C 5.8565 0.0714 5.7039 0 5.5428 0 C 5.3817 0 5.2291 0.0714 5.1268 0.1946 L 0.4468 6.3758 C 0.3286 6.5319 0.3096 6.7407 0.3977 6.9152 C 0.4858 7.0897 0.6658 7.2 0.8628 7.2 H 10.2228 C 10.4198 7.2 10.5998 7.0897 10.6879 6.9152 C 10.776 6.7407 10.757 6.5319 10.6388 6.3758 L 5.9588 0.1946 Z"
+                        fill="#FF7A68"
+                      />
+                    </svg>
+                    {percentage}%
+                  </p>
+                )
+              ) : (
+                <p className="data-source-change neutral">0%</p>
+              )}
             </div>
             <div className="data-source-content">
               <ul>
