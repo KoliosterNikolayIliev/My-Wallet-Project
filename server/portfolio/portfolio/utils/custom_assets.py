49ce2930e2c33f2cc536494e3bf7e0f193ff2928
Sheko1 <martinkypar@gmail.com>
2021-12-07 18:22:01 +0200

    added endpoint for deleting custom asset

--- server/portfolio/portfolio/utils/custom_assets.py
+++ server/portfolio/portfolio/utils/custom_assets.py
@@ -7,3 +7,9 @@ def create_asset(key, type, symbol, amount):
     body = {'type': symbol, 'amount': amount, "user-key": key}
     response = requests.post(f"{URL}api/{type}/", data=body)
     return response.json()
+
+
+def delete_asset(key, asset_type, asset):
+    body = {'asset': asset, "user-key": key}
+    response = requests.delete(f"{URL}api/{asset_type}/", data=body)
+    return response.json()
