c0af2ccd25efa56e97df33bc17a66772b2101fde
Nikolay Iliev <65191727+KoliosterNikolayIliev@users.noreply.github.com>
2021-12-09 21:49:47 +0200

    Update LoaderComponent.js

--- client/src/components/Other/LoaderComponent.js
+++ client/src/components/Other/LoaderComponent.js
@@ -1,18 +1,15 @@
 import React from "react";
 import "../../styles/loader.scss";
+
 const Loader = () => {
-    return <div className="loading">
-
-
-        <div className="container">
-            <div className="dash uno"></div>
-            <div className="dash dos"></div>
-            <div className="dash tres"></div>
-            <div className="dash cuatro"></div>
-        </div>
-
-
-    </div>;
+  return <div className="loading">
+    <div className="container">
+      <div className="dash uno"/>
+      <div className="dash dos"/>
+      <div className="dash tres"/>
+      <div className="dash cuatro"/>
+    </div>
+  </div>;
 };
 
 export default Loader;
